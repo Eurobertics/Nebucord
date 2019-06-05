de3bf8c090dc9b860c98651fd2e95130d642cf3b
Bernd Robertz <10088071+Eurobertics@users.noreply.github.com>
2019-06-05 16:52:48 +0200

    Updated version
    Updated Nebucord version

--- src/Base/Nebucord_Status.php
+++ src/Base/Nebucord_Status.php
@@ -123,7 +123,7 @@ class Nebucord_Status {
 
     const CLIENTBROWSER = 'NebucordWS API';
     const CLIENTHOST = 'nebucordws.nebulatien.org';
-    const VERSION = '0.7.7';
+    const VERSION = '0.7.8';
 
     /**
      * Returns the OS.
