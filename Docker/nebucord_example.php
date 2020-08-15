0a006c0ef8f1e05b7f0e2476545610f4c1faf0b3
Bernd Robertz <brobertz.net@gmail.com>
2020-08-15 17:43:12 +0200

    Updated Docker example file

--- Docker/nebucord_example.php
+++ Docker/nebucord_example.php
@@ -3,5 +3,11 @@ include "/opt/nebucord/vendor/autoload.php";
 
 use Nebucord\Nebucord;
 
+// Exmaple without .ini file configuration
 $n = new Nebucord(['token' => 'your-bot-token', 'ctrluser' => ['usersnowflake1', 'usersnowflake2']]);
 $n->bootstrap()->run();
+
+// Example with .ini file configuration
+// Place 'nebucord.ini' in the same directory as your main (i. e. this) start PHP file.
+$n = new Nebucord();
+$n->bootstrap('nebucord.ini', '/var/nebucord/')->run();
